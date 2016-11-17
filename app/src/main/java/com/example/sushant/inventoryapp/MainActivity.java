9f77fc9cfcb55976b086407cf7aed8174dc6473e
Rohit Karadkar <karadkar.rohit@gmail.com>
2016-11-17 23:00:53 +0530

    fix product count updation

--- app/src/main/java/com/example/sushant/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/sushant/inventoryapp/MainActivity.java
@@ -1,26 +1,20 @@
 package com.example.sushant.inventoryapp;
 
-import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Typeface;
-import android.net.Uri;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Html;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.sushant.inventoryapp.Cards.InventoryInfo;
 import com.example.sushant.inventoryapp.Cards.ProductIntroAdapter;
@@ -32,9 +26,9 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
     TextView textNoRecords;
     TextView textTitle;
-    ProductIntroAdapter productIntroAdapter;
+    ProductIntroAdapter mAdapter;
     ListView productListView;
-    ArrayList<InventoryInfo> productArrayList;
+    ArrayList<InventoryInfo> mInventoryInfoList = new ArrayList<>();
     Button productAdd;
     ImageView productThumbnail;
 
@@ -47,8 +41,8 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         productAdd=(Button)findViewById(R.id.button_add);
         productThumbnail=(ImageView)findViewById(R.id.product_thumbnail);
         productListView=(ListView)findViewById(R.id.product_list);
-        productIntroAdapter=new ProductIntroAdapter(this,R.layout.list_item_card);
-
+        mAdapter =new ProductIntroAdapter(this,R.layout.list_item_card);
+        productListView.setAdapter(mAdapter);
         InventoryHelper inventoryHelper=new InventoryHelper(getApplicationContext());
         SQLiteDatabase db=inventoryHelper.getWritableDatabase();
 
@@ -89,8 +83,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        productListView.setAdapter(productIntroAdapter);
-
         return new CursorLoader(this,
                 InventoryProvider.CONTENT_URI,
                 new String[]{"product_name","product_quantity","product_image"}, null, null, null);
@@ -98,18 +90,22 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-
         int nameColumnIndex = cursor.getColumnIndex(InventoryContracts.InventoryDetails.COLUMN_PRODUCT_NAME);
         int quantColumnIndex = cursor.getColumnIndex(InventoryContracts.InventoryDetails.COLUMN_PRODUCT_QUANTITY);
         int imageColumnIndex = cursor.getColumnIndex(InventoryContracts.InventoryDetails.COLUMN_PRODUCT_IMAGE);
 
         try {
+            mInventoryInfoList.clear();
             while (cursor.moveToNext()) {
                 InventoryInfo inventoryInfo = new InventoryInfo();
                 inventoryInfo.setProductName(cursor.getString(nameColumnIndex));
                 inventoryInfo.setProductQuantity(Integer.parseInt(cursor.getString(quantColumnIndex)));
                 inventoryInfo.setProductImage(cursor.getString(imageColumnIndex));
-                productIntroAdapter.add(inventoryInfo);
+                mInventoryInfoList.add(inventoryInfo);
+            }
+            if (mInventoryInfoList.size()>0){
+                mAdapter.clear();
+                mAdapter.addAll(mInventoryInfoList);
             }
         }
         finally {
